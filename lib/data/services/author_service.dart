46a5d730b719615f95f53f3229020ac5f61a8fae
Frank xi <yamazake@yeah.net>
2026-07-22 10:43:14 +0800

    refactor: switch AuthorService from PoetryApiClient to GatewayApiClient

--- lib/data/services/author_service.dart
+++ lib/data/services/author_service.dart
@@ -1,11 +1,11 @@
-import '../api/poetry_api_client.dart';
+import '../api/gateway_api_client.dart';
 import '../models/author.dart';
 import '../models/paginated_response.dart';
 import '../../core/constants/api_constants.dart';
 
 /// 作者数据服务
 final class AuthorService {
-  final PoetryApiClient _api = PoetryApiClient();
+  final GatewayApiClient _api = GatewayApiClient();
 
   Future<PaginatedResponse<Author>> getAuthors({
     int page = 1,
