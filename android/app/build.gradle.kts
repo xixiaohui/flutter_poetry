e0c9b42019c9545e0721814f4cb8f6c2cf9356f0
Frank xi <yamazake@yeah.net>
2026-07-24 09:55:02 +0800

    chore: unify bundle ID to com.xxh.poetry (Android + iOS)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.poetry.flutter_poetry"
+    namespace = "com.xxh.poetry"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.poetry.flutter_poetry"
+        applicationId = "com.xxh.poetry"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
